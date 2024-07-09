e3de10243943b0ff75faffee5b4f17b07242c1f3
Yousif Oday <yousefoday22@gmail.com>
2024-07-09 21:15:21 +0300

    compress files (vite)

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { VitePWA } from "vite-plugin-pwa";
+import { compression } from "vite-plugin-compression2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    compression({
+      algorithm: "brotliCompress",
+    }),
     VitePWA({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
